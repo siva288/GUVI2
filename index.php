28e4c9f97a46b0ca68f17cc1e4a816c3946d080b
siva288 <85619166+siva288@users.noreply.github.com>
2021-09-28 19:24:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <title>GUVI web developer</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../GUVI2/css/style.css">
+    <script src="../GUVI2/javascript/main.js"></script>
+</head>
+<body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 login">
+                    Username<input type="text" class="in" id="user"/>
+                    Password<input type="password" class="in" id="pass"/>
+                    <button class="btn btn-primary btns" id="login" onclick="login()">Log in</button>
+                    <a href="../GUVI2/signup.html">Sign in</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
